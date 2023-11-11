4c6d86fd1defe084d2ede8fd9628cf2abe4b1403
Chase Granberry <chase@logflare.app>
2023-11-11 13:13:19 -0700

    feat: init starting one bouncer

---
+++ pgbouncer/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the number of CPU cores
+CORES=$(nproc)
+
+# Set the auth_user password
+AUTH_USER_PASSWORD="postgres"
+
+# Create the auth_file
+echo "\"postgres\" \"$AUTH_USER_PASSWORD\"" > /opt/pgbouncer/auth_file.txt
+
+# Start the required number of PgBouncer processes
+for ((i=1;i<=CORES;i++))
+do
+ pgbouncer /opt/pgbouncer/pgbouncer.ini &
+done
+
+# Keep the script running
+wait
