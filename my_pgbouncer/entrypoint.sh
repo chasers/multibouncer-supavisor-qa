0c39a28d4d2154b1d9cc3d1c62acb1a369c028e1
Chase Granberry <chase@logflare.app>
2023-11-12 08:15:26 -0700

    fix: works with multiple pgbouncers

--- my_pgbouncer/entrypoint.sh
+++ my_pgbouncer/entrypoint.sh
@@ -2,10 +2,8 @@
 
 CORES=$(nproc)
 
-# for i in $(seq 1 $CORES); do
-#    /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini &
-# done
-
-/usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini
+for i in $(seq 1 $CORES); do
+   /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini &
+done
 
 wait
