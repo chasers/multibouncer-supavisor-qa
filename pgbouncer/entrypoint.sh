3cf945f26807ede64ce9f72e2802a8139f7e7deb
Chase Granberry <chase@logflare.app>
2023-11-13 10:28:06 -0700

    fix: connecting to Postgres working

--- pgbouncer/entrypoint.sh
+++ pgbouncer/entrypoint.sh
@@ -2,8 +2,10 @@
 
 CORES=$(nproc)
 
-for i in $(seq 1 $CORES); do
-   /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini &
-done
+# for i in $(seq 1 $CORES); do
+#    /usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini &
+# done
+
+/usr/bin/pgbouncer /etc/pgbouncer/pgbouncer.ini
 
 wait
