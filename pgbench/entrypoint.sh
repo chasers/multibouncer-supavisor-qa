a3d452d0349ca12f8ebeb3df92eceae563cb97e5
Chase Granberry <chase@logflare.app>
2023-11-13 17:31:16 -0700

    fix: keep it alive

--- pgbench/entrypoint.sh
+++ pgbench/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-pgbench --version
+# pgbench --version
+pgbench -i 'postgres://postgres:postgres@pgbouncer:6543/postgres'
+pgbench 'postgres://postgres:postgres@pgbouncer:6543/postgres' -Srn -T 60 -j 8 -c 10 -P 10 -M extended
 
-wait
+while true; do sleep 1; done
